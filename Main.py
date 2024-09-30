1bdbbde10e75308a9142688632005d9834ab0187
beorbusy <beorbusy@gmail.com>
2024-10-01 02:53:28 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import os
-from app import app as calculator_app
 
 app = Flask(__name__)
 
-# Bind to the PORT environment variable or default to 4000
 port = int(os.getenv('PORT', 4000))
 
-@app.route('/')
+
+
+
+
+app.route('/')
 def hello():
     return 'Hello World!'
-    return calculator_app.index()
+
+
+
+
+
+
 
 if __name__ == '__main__':
-    # Bind to 0.0.0.0 to allow external connections
     app.run(host='0.0.0.0', port=port)
