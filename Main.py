45d794cbfbab23c3b56d079962563ef3f6e3313c
beorbusy <beorbusy@gmail.com>
2024-10-02 00:31:02 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 from flask import Flask
 import os
-from responses import get_hello_response  # Import the function from responses.py
-from one import get_message               # Import the function from one.py
+from one import get_message  # Importing get_message() from one.py
 
 app = Flask(__name__)
 
@@ -9,8 +8,8 @@ port = int(os.getenv('PORT', 4000))
 
 @app.route('/')
 def hello():
-    message = get_message()  # Call the function from one.py
-    return f'{get_hello_response()} <br> {message}'  # Combine the HTML with the message
+    message = get_message()  # Calling the function from one.py
+    return message  # Return the message to the browser
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
