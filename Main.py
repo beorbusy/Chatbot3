882cda9f3a577fda8d2edd2b24ae9990bfce181e
beorbusy <beorbusy@gmail.com>
2024-10-02 00:42:23 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template  # Import render_template to render HTML files
 import os
 from one import get_message  # Importing get_message() from one.py
 
@@ -9,7 +9,7 @@ port = int(os.getenv('PORT', 4000))
 @app.route('/')
 def hello():
     message = get_message()  # Calling the function from one.py
-    return message  # Return the message to the browser
+    return render_template('index.html', message=message)  # Render index.html and pass the message
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
